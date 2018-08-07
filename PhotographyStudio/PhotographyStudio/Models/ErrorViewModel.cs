463add32e53fdf22b2c4ad14f2f908f3d8ac8c3a
amberlybee <amberlybee@gmail.com>
2018-08-07 15:08:11 -0600

    Initial commit

---
+++ PhotographyStudio/PhotographyStudio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhotographyStudio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
